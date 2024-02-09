a6e1a56c3b98bd44b1a0dd69c48033ed6157574f
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-02-09 14:13:03 +0800

    pseudocode and reference

---
+++ automatic_file_sorter.py
@@ -0,0 +1,8 @@
+# Automatic File Sorter - For common files of students like me :D
+# Reference: https://www.youtube.com/watch?v=gs0FNQR0njI&ab_channel=AlexTheAnalyst
+
+# Select path
+# Show the files in the path
+# List of folder names
+# Check if the path has the folders, if not, then create folders
+# Check and move the files to their corresponding folders    
