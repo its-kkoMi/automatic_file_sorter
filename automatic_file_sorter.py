bd66429bffbbab92007adf26a94f35a97a8eaf03
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-02-09 14:24:43 +0800

    Used a for loop to check if path has folders, and create if not

--- automatic_file_sorter.py
+++ automatic_file_sorter.py
@@ -15,5 +15,14 @@ print(os.listdir(path)) # This let me see the files in the path (Not Required)
 # List of folder names
 folder_names = ['Excel Files', 'Image Files', 'Text Files', 'Doc Files', 'PDF Files', 'PPT Files', 'Video Files', 'Audio Files']
 
+
+
 # Check if the path has the folders, if not, then create folders
+
+number_of_folder_names = len(folder_names) # Counts how many types of files are in the list
+
+for loop in range(0, number_of_folder_names):
+    if not os.path.exists(path + folder_names[loop]): # Checks if folder exists in path
+        os.makedirs(path + folder_names[loop])        # Creates new folder in path
+        
 # Check and move the files to their corresponding folders    
